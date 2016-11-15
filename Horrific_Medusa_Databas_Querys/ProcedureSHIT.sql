681d4a0e9865c5e61c605c69a9156589797eaa25
SofiaBamf <soffijaa@hotmail.com>
2016-11-15 10:51:04 +0100

    added procedures

--- Horrific_Medusa_Databas_Querys/ProcedureSHIT.sql
+++ Horrific_Medusa_Databas_Querys/ProcedureSHIT.sql
@@ -5,4 +5,39 @@ GO
 CREATE Procedure uspLogin @UserName nvarchar(max), @Password nvarchar(max)
 AS
 BEGIN
-	SELECT top 100 []
+	SELECT top 100 [UserID]
+	FROM Horrific_Medusa_Database.dbo.[User]
+	WHERE UserName = @UserName, [Password] = @Password
+END
+GO
+
+CREATE Procedure uspGallery @GalleryID int
+AS
+BEGIN
+	SELECT top 100 [GalleryID]
+			, [GalleryName]
+			, [PictureID]
+	FROM Horrific_Medusa_Database.dbo.[GalleryAssembly]
+	WHERE GalleryID = @GalleryID
+END 
+GO
+
+CREATE Procedure uspReservation @UserID int, @ArtistID int, @ReservationTypeID int, @StartDate datetime, @EndTime datetime
+AS
+BEGIN
+	SELECT top 100 [ReservationID]
+	FROM Horrific_Medusa_Database.dbo.[Reservation]
+	WHERE UserID = @UserID, ArtistID = @ArtistID, ReservationTypeID = @ReservationTypeID, StartDate = @StartDate, EndTime = @EndTime
+END
+GO
+
+-- CREATE Procedure uspRegistration @UserName nvarchar(max), @Password nvarchar(max), @FirstName 
+
+CREATE Procedure uspConfirmation @ReservationID int
+AS
+BEGIN
+	SELECT top 100 [ConfirmationID]
+			, [StartDate]
+			, [EndTime]
+	FROM Horrific_Medusa_Database.dbo.[Reservation
+	WHERE 
