8c2d1affa35fde676b2c141750492a1cbb41d947
SofiaBamf <soffijaa@hotmail.com>
2016-11-22 11:16:46 +0100

    finished procedurestuff mayebe, and made changes in schememini

--- Horrific_Medusa_Databas_Querys/ProcedureSHIT.sql
+++ Horrific_Medusa_Databas_Querys/ProcedureSHIT.sql
@@ -5,55 +5,56 @@ GO
 CREATE Procedure uspLogin @UserName nvarchar(max), @Password nvarchar(max)
 AS
 BEGIN
-	SELECT top 100 [UserID]
-	FROM Horrific_Medusa_Database.dbo.[User]
-	WHERE UserName = @UserName And [Password] = @Password
+	SELECT top 100 U.UserID
+	FROM Horrific_Medusa_Database.dbo.[User] AS U
+	WHERE U.UserName = @UserName And U.[Password] = @Password
 END
 GO
 
 CREATE Procedure uspGallery @GalleryID int
 AS
 BEGIN
-	SELECT top 100 [GalleryID]
-			, [PictureID]
-	FROM Horrific_Medusa_Database.dbo.[GalleryAssembly]
-	WHERE GalleryID = @GalleryID
-	SELECT top 100 [GalleryName]
-	FROM Horrific_Medusa_Database.dbo.[Gallery]
-
-
+	SELECT top 100 GA.GalleryID
+			, GA.PictureID
+			, G.GalleryName
+	FROM Horrific_Medusa_Database.dbo.GalleryAssembly AS GA
+	JOIN Horrific_Medusa_Database.dbo.Gallery G ON GA.GalleryID = G.GalleryID
+	WHERE GA.GalleryID = @GalleryID
 END 
 GO
 
-CREATE Procedure uspReservation @UserID int, @ArtistID int, @ReservationTypeID int, @StartDate datetime, @EndTime datetime
+CREATE Procedure uspReservation @UserID int, @ArtistID int, 
+	@ReservationTypeID int, @StartDate datetime, @EndTime datetime
 AS
 BEGIN
-	SELECT top 100 [ReservationID]
-	FROM Horrific_Medusa_Database.dbo.[Reservation]
-	WHERE UserID = @UserID And ArtistID = @ArtistID And ReservationTypeID = @ReservationTypeID And StartDate = @StartDate And EndTime = @EndTime
+	SELECT top 100 RE.ReservationID
+	FROM Horrific_Medusa_Database.dbo.Reservation AS RE
+	WHERE RE.UserID = @UserID And RE.ArtistID = @ArtistID And 
+		RE.ReservationTypeID = @ReservationTypeID And RE.StartDate = @StartDate 
+		And RE.EndTime = @EndTime
 END
 GO
 
 CREATE Procedure uspConfirmation @ReservationID int
 AS
 BEGIN
-	SELECT top 100 [ConfirmationID]
-	FROM Horrific_Medusa_Database.dbo.[Confirmation]
-	SELECT top 100 [StartDate]
-			, [EndTime]
-	FROM Horrific_Medusa_Database.dbo.[Reservation]
-	WHERE ReservationID = @ReservationID
+	SELECT top 100 C.ConfirmationID
+			, R.StartDate
+			, R.EndTime
+	FROM Horrific_Medusa_Database.dbo.Confirmation AS C
+	JOIN Horrific_Medusa_Database.dbo.Reservation R ON C.ReservationID = R.ReservationID
+	WHERE C.ReservationID = @ReservationID
 END
 GO
 
 CREATE Procedure uspReminder @ReservationID int
 AS
 BEGIN
-	SELECT top 100 [RiminderID]
-	FROM Horrific_Medusa_Database.dbo.[Reminder]
-	SELECT top 100 [StartDate]
-			, [EndTime]
-	FROM Horrific_Medusa_Database.dbo.[Reservation]
+	SELECT top 100 REM.RiminderID
+			, RE.StartDate
+			, RE.EndTime
+	FROM Horrific_Medusa_Database.dbo.Reminder AS REM
+	JOIN Horrific_Medusa_Database.dbo.Reservation RE ON REM.ResverationID = RE.ReservationID
 	WHERE ReservationID = @ReservationID
 END
 GO
