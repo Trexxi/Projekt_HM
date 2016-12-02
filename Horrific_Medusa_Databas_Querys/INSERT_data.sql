df119ae6373fc685daa3497db7f3b2a0fa6a45bf
Teddy <bibbis11@hotmail.com>
2016-12-02 13:04:31 +0100

    databasskit

--- Horrific_Medusa_Databas_Querys/INSERT_data.sql
+++ Horrific_Medusa_Databas_Querys/INSERT_data.sql
@@ -61,6 +61,30 @@ INSERT INTO Reservation (UserID, ArtistID, ReservationTypeID) VALUES (4, 2, 1)
 INSERT INTO Reservation (UserID, ArtistID, ReservationTypeID) VALUES (5, 2, 1)
 INSERT INTO Reservation (UserID, ArtistID, ReservationTypeID) VALUES (6, 3, 1)
 INSERT INTO Reservation (UserID, ArtistID, ReservationTypeID) VALUES (7, 3, 1)
+
+INSERT INTO Confirmation(ReservationID) VALUES (1)
+INSERT INTO Confirmation(ReservationID) VALUES (3)
+INSERT INTO Confirmation(ReservationID) VALUES (10)
+INSERT INTO Confirmation(ReservationID) VALUES (2)
+
+INSERT INTO ConfirmationAssembly(ConfirmationID, ConfirmationTypeID) VALUES (1, 1)
+INSERT INTO ConfirmationAssembly(ConfirmationID, ConfirmationTypeID) VALUES (1, 2)
+INSERT INTO ConfirmationAssembly(ConfirmationID, ConfirmationTypeID) VALUES (2, 1)
+INSERT INTO ConfirmationAssembly(ConfirmationID, ConfirmationTypeID) VALUES (3, 2)
+INSERT INTO ConfirmationAssembly(ConfirmationID, ConfirmationTypeID) VALUES (4, 1)
+INSERT INTO ConfirmationAssembly(ConfirmationID, ConfirmationTypeID) VALUES (4, 2)
+
+INSERT INTO Reminder(ResverationID) VALUES (1)
+INSERT INTO Reminder(ResverationID) VALUES (3)
+INSERT INTO Reminder(ResverationID) VALUES (10)
+INSERT INTO Reminder(ResverationID) VALUES (2)
+
+INSERT INTO ReminderAssembly(ReminderID, ReminderTypID) VALUES (1, 1)
+INSERT INTO ReminderAssembly(ReminderID, ReminderTypID) VALUES (1, 2)
+INSERT INTO ReminderAssembly(ReminderID, ReminderTypID) VALUES (2, 1)
+INSERT INTO ReminderAssembly(ReminderID, ReminderTypID) VALUES (3, 2)
+INSERT INTO ReminderAssembly(ReminderID, ReminderTypID) VALUES (4, 1)
+INSERT INTO ReminderAssembly(ReminderID, ReminderTypID) VALUES (4, 2)
 /** Kör filen INSERT_DATA 2 **/
 
 
--- Horrific_Medusa_Databas_Querys/INSERT_data2.sql
+++ Horrific_Medusa_Databas_Querys/INSERT_data2.sql
@@ -1,7 +1,18 @@
 use Horrific_Medusa_Database
 GO
-SELECT [SchemeID]
-      ,[AvailableTime]
+
+INSERT INTO [dbo].[Scheme]
+           ([AvailableTime]
+           ,[InformationBox]
+           ,[ArtistID]
+           ,[SchemeStartDate]
+           ,[SchemeEndDate]
+           ,[ReservationID]
+           ,[CreatedDate]
+           ,[CreatedBy]
+           ,[UpdatedDate]
+           ,[UpdatedBy])
+SELECT [AvailableTime]
       ,[InformationBox]
       ,[ArtistID]
       ,[SchemeStartDate]
@@ -11,33 +22,8 @@ SELECT [SchemeID]
       ,[CreatedBy]
       ,[UpdatedDate]
       ,[UpdatedBy]
-	  into dbo.Scheme
+
   FROM[Kollpas].dbo.Schemesak
-GO
-
-INSERT INTO Confirmation(ReservationID) VALUES (1)
-INSERT INTO Confirmation(ReservationID) VALUES (3)
-INSERT INTO Confirmation(ReservationID) VALUES (10)
-INSERT INTO Confirmation(ReservationID) VALUES (2)
-
-INSERT INTO ConfirmationAssembly(ConfirmationID, ConfirmationTypeID) VALUES (1, 1)
-INSERT INTO ConfirmationAssembly(ConfirmationID, ConfirmationTypeID) VALUES (1, 2)
-INSERT INTO ConfirmationAssembly(ConfirmationID, ConfirmationTypeID) VALUES (2, 1)
-INSERT INTO ConfirmationAssembly(ConfirmationID, ConfirmationTypeID) VALUES (3, 2)
-INSERT INTO ConfirmationAssembly(ConfirmationID, ConfirmationTypeID) VALUES (4, 1)
-INSERT INTO ConfirmationAssembly(ConfirmationID, ConfirmationTypeID) VALUES (4, 2)
-
-INSERT INTO Reminder(ResverationID) VALUES (1)
-INSERT INTO Reminder(ResverationID) VALUES (3)
-INSERT INTO Reminder(ResverationID) VALUES (10)
-INSERT INTO Reminder(ResverationID) VALUES (2)
-
-INSERT INTO ReminderAssembly(ReminderID, ReminderTypID) VALUES (1, 1)
-INSERT INTO ReminderAssembly(ReminderID, ReminderTypID) VALUES (1, 2)
-INSERT INTO ReminderAssembly(ReminderID, ReminderTypID) VALUES (2, 1)
-INSERT INTO ReminderAssembly(ReminderID, ReminderTypID) VALUES (3, 2)
-INSERT INTO ReminderAssembly(ReminderID, ReminderTypID) VALUES (4, 1)
-INSERT INTO ReminderAssembly(ReminderID, ReminderTypID) VALUES (4, 2)
 
 
 
