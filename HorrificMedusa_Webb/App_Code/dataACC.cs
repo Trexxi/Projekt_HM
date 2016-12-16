0a99b1ad640d17aa0e2d41680b192e89eae3b960
Teddy <bibbis11@hotmail.com>
2016-12-16 14:19:20 +0100

    Scheme working kind of

---
+++ HorrificMedusa_Webb/App_Code/dataACC.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for dataACC
+/// </summary>
+public class dataACC
+{
+    string connstr = ConfigurationManager.ConnectionStrings["MedusaConnectionString"].ToString();
+    public dataACC()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public DataTable getScheme(int WeekNumber, int ArtistID)
+    {
+        DataTable dt = new DataTable();
+
+        SqlConnection sqlcon = new SqlConnection(connstr);
+
+        SqlCommand cmd = new SqlCommand("uspScheme", sqlcon);
+
+        cmd.CommandType = CommandType.StoredProcedure;
+
+        try
+        {
+            sqlcon.Open();
+            cmd.Parameters.AddWithValue("@WeekNumber", WeekNumber);
+            cmd.Parameters.AddWithValue("@Artist", ArtistID);
+            dt.Load(cmd.ExecuteReader());
+            return dt;
+
+        }
+        catch
+        {
+            throw;
+        }
+        finally
+        {
+            cmd.Dispose();
+            sqlcon.Close();
+            sqlcon.Dispose();
+        }
+    }
+}
