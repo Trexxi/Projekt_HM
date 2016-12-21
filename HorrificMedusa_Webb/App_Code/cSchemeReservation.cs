5b6acfee441841b16a08f702e2b6c7f6eca87c13
Teddy <bibbis11@hotmail.com>
2016-12-21 09:34:53 +0100

    dataAcc and Reservation

---
+++ HorrificMedusa_Webb/App_Code/cSchemeReservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for cSchemeReservation
+/// </summary>
+public class cSchemeReservation
+{
+    public cSchemeReservation()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public string ArtistName { get; set; }
+    public string InformationBox { get; set; }
+    public DateTime SchemeStartDate { get; set; }
+    public DateTime SchemeEndDate { get; set; }
+}
