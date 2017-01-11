e8c301ed0d99b5331d31efeb923895ea702bc4f3
SofiaBamf <soffijaa@hotmail.com>
2017-01-11 12:31:31 +0100

    not much

--- HorrificMedusa_Webb/ReservationPage.aspx.cs
+++ HorrificMedusa_Webb/ReservationPage.aspx.cs
@@ -53,11 +53,15 @@ public partial class ReservationPage : System.Web.UI.Page
         cDAL dal = new cDAL();
         myUser = dal.PersonalInformation(UserId);
 
-        if (myUser.Date != null)
-
+        if (myUser.UserId > 0)
+        {
             btnLogIn.Visible = false;
-        btnRegistry.Visible = false;
-        btnLogOut.Visible = true;
+            btnRegistry.Visible = false;
+            btnLogOut.Visible = true;
+
+        }
+
+            
     }
 
     protected void ddlArtist_SelectedIndexChanged(object sender, EventArgs e)
